87e166cfbb6f6449d3d62e61f0ff6a0d02272bb5
świdur <dawid@swidurski.pl>
2020-10-21 11:21:18 +0200

    Dodaj pliki projektów.

---
+++ LinkShortenerWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LinkShortenerWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
